7877a7414f9c521c81a90ad2a5d50005fdc936b5
anmamun0 <anmamun0@gmail.com>
2025-10-16 17:32:07 +0600

    _

--- VTS_Python_Backend/drf-practice/day_1/core/accounts/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from django.contrib.auth import get_user_model
-User = get_user_model()
-
-class UserSerializers(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = ['username','first_name','last_name','password','email','bio','designation']
