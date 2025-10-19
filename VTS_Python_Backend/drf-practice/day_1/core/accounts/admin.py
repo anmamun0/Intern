0a9653c2611a0171715864fd94c7a2731639f216
anmamun0 <anmamun0@gmail.com>
2025-10-19 16:46:49 +0600

    django-rest-framework  REST Permissions > Built-in Permissions

--- VTS_Python_Backend/drf-practice/day_1/core/accounts/admin.py
+++ VTS_Python_Backend/drf-practice/day_1/core/accounts/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Student,Links
+from .models import Student,Links,CustomUser
 
 admin.site.register(Student)
 admin.site.register(Links)
+admin.site.register(CustomUser)
--- VTS_Python_Backend/drf-practice/day_1/core/accounts/permissions.py
+++ VTS_Python_Backend/drf-practice/day_1/core/accounts/permissions.py
@@ -1,8 +1,11 @@
 from rest_framework.permissions import AllowAny,IsAuthenticated,IsAdminUser,IsAuthenticatedOrReadOnly
-# AllowAny = Any use can access authenticated or unauthenticated
-# IsAuthenticated = Just access authenticated user request
 
-# IsAdminUser = Just Access can admin / staff user request
+"""
+# API Request Permission
 
-# IsAuthenticatedOrReadOnly = GET Request Allow any user | But [POST, PUT, PATCH, DELETE] authenticated user can access 
- 
+AllowAny        # Any use can access authenticated or unauthenticated
+IsAuthenticated # Just access authenticated user request
+IsAdminUser     # Just Access can admin / staff user request
+IsAuthenticatedOrReadOnly # GET Request Allow any user | But [POST, PUT, PATCH, DELETE] authenticated user can access  
+
+"""
