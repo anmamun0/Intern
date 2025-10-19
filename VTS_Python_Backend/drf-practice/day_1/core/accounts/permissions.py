a3724a1a0ab6b55cc6208e9932a5e73227fd9306
anmamun0 <anmamun0@gmail.com>
2025-10-19 16:48:37 +0600

    django-rest-framework  REST Permissions > Model Class Permission

--- VTS_Python_Backend/drf-practice/day_1/core/accounts/permissions.py
+++ VTS_Python_Backend/drf-practice/day_1/core/accounts/permissions.py
@@ -9,3 +9,16 @@ IsAdminUser     # Just Access can admin / staff user request
 IsAuthenticatedOrReadOnly # GET Request Allow any user | But [POST, PUT, PATCH, DELETE] authenticated user can access  
 
 """
+
+# Model Class Permission
+"""
+Django-তে প্রতিটি model এর জন্য ৪টা permission থাকে:
+add_modelname,change_modelname, delete_modelname, view_modelname
+
+
+DjangoModelPermissions                  #
+DjangoModelPermissionsOrAnonReadOnly    #  just can access view_model ,
+"""
+
+
+
